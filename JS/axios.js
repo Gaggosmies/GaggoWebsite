9781cdca466746186d476b0457de7a1fde4a3ec6
Gaggosmies <Gaggosmies@hotmail.com>
2022-04-09 21:52:33 +0300

    Changed the search to be done with search status instead of sheet tab

--- JS/axios.js
+++ JS/axios.js
@@ -1,30 +1,40 @@
-var APIurl = 'https://sheetdb.io/api/v1/vehbaa8eppm85?sheet=Accepted';
+var APIurl = 'https://sheetdb.io/api/v1/vehbaa8eppm85';
 const date = new Date();
-var dateNow = date.getFullYear().toString() + "/" + ('0' + (date.getMonth() +1)).slice(-2).toString() + "/" + ('0' + date.getDate()).slice(-2).toString() + "-"+ date.getHours().toString() + ':' + date.getMinutes().toString();
+var dateNow = date.getFullYear().toString() + "/" + ('0' + (date.getMonth() + 1)).slice(-2).toString() + "/" + ('0' + date.getDate()).slice(-2).toString() + "-" + date.getHours().toString() + ':' + date.getMinutes().toString();
 
-// axios.get(APIurl)
-//     .then(function (response) {
-//         console.log(response);
-//     })
-//     .catch(function (error) {
-//         console.log(error);
-//     })
-//     .finally(function () {
-//         // Always executed
-//     });
+axios.get(APIurl + '/search?Status=Done')
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+    .finally(function () {
+        // Always executed
+    });
 
 axios.post(APIurl, {
     "data": {
+        "ID": 1,
         "Username": "Gaggotest",
-        "Quality": "Shaded", 
-        "Character Quantity": "Bad", 
-        "Background": "Lfol", 
+        "Quality": "Shaded",
+        "Character Quantity": "Bad",
+        "Background": "Lfol",
         "Price": 0,
-        "Date": dateNow
+        "Date": dateNow,
+        "Status": "Done"
     }
 }).then(response => {
     console.log(response.data);
 });
+
+// axios.patch(APIurl + '/ID/1', {
+//     "data": {
+//         "Status": "Not done"
+//     }
+// }).then(response => {
+//     console.log(response.data);
+// });
 
 // axios.delete(APIurl + '/ID/2')
 //     .then(response => {
